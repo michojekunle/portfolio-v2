5ad99554361764356cad56888e6d1a09e27ee795
AMD <michojekunle1@gmail.com>
2026-03-31 23:52:52 +0100

    fix: update API routes

--- app/api/guestbook/route.ts
+++ app/api/guestbook/route.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 function getSupabase() {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.SUPABASE_KEY!
   );
 }
 
